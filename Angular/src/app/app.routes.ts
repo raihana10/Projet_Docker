26870c8b9eba5b21d0b9efd58e1ef3968abf02ec
ismail <ismaillyamani4@gmail.com>
2025-04-24 10:44:15 +0000

    Angular

---
+++ Angular/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
