f1e8c6e24ffe88566f0ee3268a61814e09578ad5
douaem1 <douae.moeniss@gmail.com>
2025-04-26 17:23:02 +0100

    ?

--- backend/storage/framework/views/00e851ac95195591f801b373a1c7ae61.php
+++ backend/storage/framework/views/00e851ac95195591f801b373a1c7ae61.php
@@ -6,16 +6,43 @@
     <title>Document</title>
 </head>
 <body>
-<div style ="border: 3px solid black;">
-        <h2>Registrer</h2>
-            <form action="/register" method="POST">
-                <?php echo csrf_field(); ?>
-                <input name="name" type="text" placeholder="nom">
-                <input name="email" type="text" placeholder="email_institutionnelle">
-                <input name="password" type="password" placeholder="password">
-                <button>register</button>
-            </form>
-        
+    <?php if(auth()->guard()->check()): ?>
+    <p>vou etes connécte</p>
+    <form action="/logout" method="POST">
+        <?php echo csrf_field(); ?>
+        <button>deconnexion</button>
+    </form>
+    <div style ="border: 3px solid black;">
+        <h2>Ajouter un dette</h2>
+        <form action="/creat-dette" method="POST">
+            <?php echo csrf_field(); ?>
+            <input name="nom" type="text" placeholder="nom">
+            <input name="prix" type="text" placeholder="prix">
+            <input name="description" type="text" placeholder="description">
+            <button>ajouter</button>
+        </form>
     </div>
+    <?php else: ?> 
+    <div style ="border: 3px solid black;">
+        <h2>Registrer</h2>
+        <form action="/register" method="POST">
+            <?php echo csrf_field(); ?>
+            <input name="nom" type="text" placeholder="nom">
+            <input name="email" type="text" placeholder="email">
+            <input name="mot_de_passe" type="password" placeholder="mot de passe">
+            <button>register</button>
+        </form>
+    </div>
+    <div style ="border: 3px solid black;">
+        <h2>login</h2>
+        <form action="/login" method="POST">
+            <?php echo csrf_field(); ?>
+            <input name="loginnom" type="text" placeholder="nom">
+            <input name="loginmot_de_passe" type="password" placeholder="mot de passe">
+            <button>login</button>
+        </form>
+    </div>
+    <?php endif; ?>
+    
 </body>
 </html><?php /**PATH /var/www/resources/views/test.blade.php ENDPATH**/ ?>
