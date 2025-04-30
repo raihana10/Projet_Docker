c086d5bbc6329250c650c1e1c32ef267df3e072f
Ismail Lyamani <ismaillyamani4@gmail.com>
2025-04-30 02:32:04 +0100

    test2 controller

--- app/Http/Controllers/test2Controller.php
+++ app/Http/Controllers/test2Controller.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class test2Controller extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        return view('test2', compact('user'));
+    }
 }
