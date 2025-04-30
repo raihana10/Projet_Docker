22a09063e71ddd14b2aa630666dbf1fbbbf248a9
Ismail Lyamani <ismaillyamani4@gmail.com>
2025-04-30 02:27:53 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DebtController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\test2Controller;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test');
 });
+
+Route::post('/register', [UserController::class,'register']);
+Route::get('/test2', [test2Controller::class, 'index'])->middleware('auth');
+Route::post('/logout', [UserController::class,'logout']); // Décommenter cette ligne
+Route::post('/login', [UserController::class,'login']);
+// Route::post('/create-dette', [debtController::class,'createDebt']);
+Route::get('/profile', function () {
+    return view('profile', ['user' => auth()->user()]);
+})->middleware('auth');
+Route::post('/profile/update', [App\Http\Controllers\UserController::class, 'updateProfile'])->middleware('auth');
